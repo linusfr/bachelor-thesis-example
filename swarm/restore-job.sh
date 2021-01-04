1c2a739985edd763c01f043b66a74d407edaf4bb
Linus <linus.frotscher@zenner-iot.com>
2021-01-04 12:31:13 +0100

    rework restore job

--- swarm/restore-job.sh
+++ swarm/restore-job.sh
@@ -2,17 +2,19 @@ docker service create   --name restore-cats \
                         --mode replicated-job \
                         --replicas 1 \
                         --max-concurrent 1 \
-                        --network cats_default \
+                        --network cats_cats \
                         --env PGHOST=db \
                         --config cats_postgres-user \
                         --config cats_postgres-db \
                         --secret cats_postgres-password \
                         --mount 'type=volume,dst=/backup,volume-driver=local,volume-opt=type=nfs,volume-opt=device=:/srv/nfs/postgres-backup-cats-swarm,"volume-opt=o=addr=192.168.178.35,nfsvers=4,rw"' \
-                          postgres:alpine bash -c 'export PGUSER=$(cat /cats_postgres-user) PGDATABASE=$(cat /cats_postgres-db) && echo "*:*:*:*:$(cat /run/secrets/cats_postgres-password)" > ~/.pgpass \
-                            && chmod 0600 ~/.pgpass \
-                            && pg_restore \
-                              --verbose \
-                              --no-owner \
-                              --clean \
-                              --if-exists \
-                              --dbname=${PGDATABASE} /backup/${PGDATABASE}.sql.gz'
+                        postgres:alpine bash -c '
+                          echo $(</run/secrets/cats_postgres-password) | \
+                          pg_restore \
+                            --username=$(</cats_postgres-user) \
+                            --verbose \
+                            --no-owner \
+                            --clean \
+                            --if-exists \
+                            --password \
+                            --dbname=$(</cats_postgres-db) /backup/$(</cats_postgres-db).sql.gz'
